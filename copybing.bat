6059417f21d940b929b68c01adfead4d71521854
Paul Smith <paul@technostuff.net>
2017-06-26 05:43:01 -0600

    Don't hardcode user directory in copybing

--- copybing.bat
+++ copybing.bat
@@ -1,7 +1,7 @@
 @REM This batch file copies the bing desktop wallpapers from the temp directory to a save directory.
 if NOT exist c:\temp\wallpapers mkdir c:\temp\wallpapers
-copy/y "C:\Users\Paul\AppData\Local\Microsoft\BingDesktop\en-US\Apps\Wallpaper_5386c77076d04cf9a8b5d619b4cba48e\VersionIndependent\images\*.jpg" c:\temp\wallpapers
+copy/y "%USERPROFILE%\AppData\Local\Microsoft\BingDesktop\en-US\Apps\Wallpaper_5386c77076d04cf9a8b5d619b4cba48e\VersionIndependent\images\*.jpg" c:\temp\wallpapers
 
 @REM these are the lock screen wallpapers
-copy/y "C:\Users\Paul\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" c:\temp\wallpapers
+copy/y "%USERPROFILE%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" c:\temp\wallpapers
 
