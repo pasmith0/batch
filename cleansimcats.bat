82eebaef5e6addd871978d77f69b1e0cc20f342c
Paul Smith <paul@psmith.me>
2018-07-27 07:55:27 -0600

    Reorg part 2

--- cleansimcats.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del/q C:\SimInternalStorageproxy\odnDir_0\catalogs\*
-if exist C:\simRecordings del/q C:\simRecordings
-if exist C:\SimInternalStorageproxy\odnDir_0\odnAppDataMap del/q C:\SimInternalStorageproxy\odnDir_0\odnAppDataMap
-if exist C:\SimInternalStorageproxy\odnDir_0\odnSchedule   del/q C:\SimInternalStorageproxy\odnDir_0\odnSchedule
-if exist C:\SimInternalStorageproxy\odnDir_0\odnSeries     del/q C:\SimInternalStorageproxy\odnDir_0\odnSeries
